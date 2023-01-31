987fc297a67bb3476a952e3d943d0cdfe4021516
Alexander Widmer <alexander.widmer@me.com>
2023-01-31 20:51:53 +0100

    feat(global): installed lobsome component library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,16 @@
-import { GetServerSideProps, InferGetStaticPropsType } from "next";
-import { Header } from "../components/header";
+import {GetServerSideProps, InferGetStaticPropsType} from "next";
+import {
+    Button,
+    ButtonColors,
+    Card,
+    Heading,
+    HeadingTags,
+    Paragraph,
+    ParagraphSizes,
+    SvgSend,
+    SvgUpload,
+    Textarea
+} from "@smartive-education/design-system-component-library-lobsome";
 
 type PageProps = {};
 
@@ -8,9 +19,39 @@ export default function PageHome({}: PageProps): InferGetStaticPropsType<
 > {
   return (
     <>
-      <Header title="Mumble">
-        <span>Your custom network</span>
-      </Header>
+      <Heading tag={HeadingTags.HEADING3}>Heading</Heading>
+
+        <div className="bg-slate-100 p-10">
+            <div className="w-[680px]">
+                <Card>
+                    <Heading tag={HeadingTags.HEADING3}>
+                        Voll leer hier! 😲
+                    </Heading>
+                    <Paragraph size={ParagraphSizes.m}>
+                        Verfasse deinen ersten Mumble oder folge anderen Usern!
+                    </Paragraph>
+                    <div className="mt-8 mb-4">
+                        <Textarea placeholder="Und was meinst du dazu?" />
+                    </div>
+                    <div className="flex space-x-5">
+                        <Button
+                            color={ButtonColors.SLATE}
+                            fullWidth
+                        >
+                            Bild hochladen{' '}
+                            <SvgUpload />
+                        </Button>
+                        <Button
+                            color={ButtonColors.VIOLET}
+                            fullWidth
+                        >
+                            Absenden{' '}
+                            <SvgSend />
+                        </Button>
+                    </div>
+                </Card>
+            </div>
+        </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [
+    require('@smartive-education/design-system-component-library-lobsome/preset')
+  ],
   content: [
     "./app/**/*.{ts,tsx}",
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
+    "./node_modules/@smartive-education/design-system-component-library-lobsome/dist/esm/components/**/*.js"
   ],
   theme: {
     extend: {},
