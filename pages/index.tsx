68e427b7ebb1082dcbda63b1725f0a4a843c1d51
Milos Djuric <milos.djuric@ost.ch>
2023-02-14 18:08:31 +0100

    feat(global): added basic translation functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,69 @@
-import {GetServerSideProps, InferGetStaticPropsType} from "next";
+import { GetServerSideProps, InferGetStaticPropsType } from "next";
 import {
-    Button,
-    ButtonColors,
-    Card,
-    Heading,
-    HeadingTags,
-    Paragraph,
-    ParagraphSizes,
-    SvgSend,
-    SvgUpload,
-    Textarea
+  Button,
+  ButtonColors,
+  ButtonSizes,
+  Card,
+  Heading,
+  HeadingTags,
+  Paragraph,
+  ParagraphSizes,
+  SvgSend,
+  SvgUpload,
+  Textarea,
 } from "@smartive-education/design-system-component-library-lobsome";
+import useTranslation from "next-translate/useTranslation";
+import setLanguage from "next-translate/setLanguage";
 
 type PageProps = {};
 
 export default function PageHome({}: PageProps): InferGetStaticPropsType<
   typeof getServerSideProps
 > {
+  const { t, lang } = useTranslation("index");
+  const title = t("title");
+
   return (
     <>
-      <Heading tag={HeadingTags.HEADING3}>Heading</Heading>
+      <Button
+        color={ButtonColors.VIOLET}
+        size={ButtonSizes.M}
+        onClick={async () => await setLanguage("en")}
+      >
+        EN
+      </Button>
 
-        <div className="bg-slate-100 p-10">
-            <div className="w-[680px]">
-                <Card>
-                    <Heading tag={HeadingTags.HEADING3}>
-                        Voll leer hier! 😲
-                    </Heading>
-                    <Paragraph size={ParagraphSizes.m}>
-                        Verfasse deinen ersten Mumble oder folge anderen Usern!
-                    </Paragraph>
-                    <div className="mt-8 mb-4">
-                        <Textarea placeholder="Und was meinst du dazu?" />
-                    </div>
-                    <div className="flex space-x-5">
-                        <Button
-                            color={ButtonColors.SLATE}
-                            fullWidth
-                        >
-                            Bild hochladen{' '}
-                            <SvgUpload />
-                        </Button>
-                        <Button
-                            color={ButtonColors.VIOLET}
-                            fullWidth
-                        >
-                            Absenden{' '}
-                            <SvgSend />
-                        </Button>
-                    </div>
-                </Card>
+      <Button
+        color={ButtonColors.VIOLET}
+        size={ButtonSizes.M}
+        onClick={async () => await setLanguage("de")}
+      >
+        DE
+      </Button>
+
+      <Heading tag={HeadingTags.HEADING3}>{title}</Heading>
+
+      <div className="bg-slate-100 p-10">
+        <div className="w-[680px]">
+          <Card>
+            <Heading tag={HeadingTags.HEADING3}>Voll leer hier! 😲</Heading>
+            <Paragraph size={ParagraphSizes.m}>
+              Verfasse deinen ersten Mumble oder folge anderen Usern!
+            </Paragraph>
+            <div className="mt-8 mb-4">
+              <Textarea placeholder="Und was meinst du dazu?" />
             </div>
+            <div className="flex space-x-5">
+              <Button color={ButtonColors.SLATE} fullWidth>
+                Bild hochladen <SvgUpload />
+              </Button>
+              <Button color={ButtonColors.VIOLET} fullWidth>
+                Absenden <SvgSend />
+              </Button>
+            </div>
+          </Card>
         </div>
+      </div>
     </>
   );
 }
