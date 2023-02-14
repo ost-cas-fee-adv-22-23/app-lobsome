2e7b66e3b8fa49d53e4773959487f059c738b87a
Alexander Widmer <alexander.widmer@me.com>
2023-02-14 20:20:40 +0100

    feat(login): added signIn click handler for zitadel

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,81 +1,78 @@
 import {
-    Button,
-    ButtonColors,
-    ButtonSizes,
-    Heading,
-    HeadingColors,
-    Input,
-    Label,
-    LabelSizes,
-    Link,
-    SvgEye,
-    SvgMumble
+  Button,
+  ButtonColors,
+  ButtonSizes,
+  Heading,
+  HeadingColors,
+  Input,
+  Label,
+  LabelSizes,
+  Link,
+  SvgEye,
+  SvgMumble,
 } from "@smartive-education/design-system-component-library-lobsome";
+import { signIn } from "next-auth/react";
 
 export default function Home() {
-    // const {data: session} = useSession();
+  // const {data: session} = useSession();
 
-    return (
-        <>
-            <div className="flex items-stretch bg-grey-lighter min-h-screen">
-                <div
-                    className="bg-gradient-to-br from-pink-500 to-violet-600 flex-1 text-grey-darker text-center bg-grey-light ">
-                    <div className="flex items-center justify-center h-screen">
-                        <Heading color={HeadingColors.WHITE}>Find out what’s new in <span>#fashion</span></Heading>
-                    </div>
-                </div>
-                <div className="flex-1 text-grey-darker text-center bg-grey-light ">
+  return (
+    <>
+      <div className="flex items-stretch bg-grey-lighter min-h-screen">
+        <div className="bg-gradient-to-br from-pink-500 to-violet-600 flex-1 text-grey-darker text-center bg-grey-light ">
+          <div className="flex items-center justify-center h-screen">
+            <Heading color={HeadingColors.WHITE}>
+              Find out what’s new in <span>#fashion</span>
+            </Heading>
+          </div>
+        </div>
+        <div className="flex-1 text-grey-darker bg-grey-light ">
+          <div className="flex items-center justify-center h-screen">
+            <div className="space-y-2">
+              <Heading color={HeadingColors.SLATE}>Anmelden</Heading>
+              <Input label="E-Mail"></Input>
+              <Input type="password" label="Password">
+                <SvgEye />
+              </Input>
 
-                    <div className="flex items-center justify-center h-screen">
-                        <div className="space-y-2">
-                            <Heading color={HeadingColors.SLATE}>Anmelden</Heading>
-                            <Input
-                                label="E-Mail"
-                            >
-                            </Input>
-                            <Input
-                                label="Password"
-                            >
-                                <SvgEye/>
-                            </Input>
+              <input type="text" />
 
-                            <Button
-                                color={ButtonColors.GRADIENT}
-                                label="Let's mumble"
-                                size={ButtonSizes.M}
-                                fullWidth={true}
-                            >
-                                <SvgMumble/>
-                            </Button>
-                            <Label size={LabelSizes.s}>Noch kein Account?</Label>
-                            <Link hasUnderline={true}> Jetzt registrieren</Link>
-                        </div>
-                    </div>
-
-                </div>
+              <Button
+                color={ButtonColors.GRADIENT}
+                label="Let's mumble"
+                size={ButtonSizes.M}
+                fullWidth={true}
+                onClick={() => signIn("zitadel", { callbackUrl: '/' })}
+              >
+                <SvgMumble />
+              </Button>
+              <Label size={LabelSizes.s}>Noch kein Account?</Label>
+              <Link hasUnderline={true}> Jetzt registrieren</Link>
             </div>
+          </div>
+        </div>
+      </div>
 
+      {/*<Head>*/}
+      {/*  <title>Login</title>*/}
+      {/*  <link rel="icon" href="/favicon.ico" />*/}
+      {/*</Head>*/}
 
-            {/*<Head>*/}
-            {/*  <title>Login</title>*/}
-            {/*  <link rel="icon" href="/favicon.ico" />*/}
-            {/*</Head>*/}
+      {/*<main>*/}
+      {/*  {!!session && (*/}
+      {/*    <a href="#" onClick={() => signOut()}>*/}
+      {/*      <h2>Logout &rarr;</h2>*/}
+      {/*      <p>Logout from your account</p>*/}
+      {/*    </a>*/}
+      {/*  )}*/}
 
-            {/*<main>*/}
-            {/*  {!!session && (*/}
-            {/*    <a href="#" onClick={() => signOut()}>*/}
-            {/*      <h2>Logout &rarr;</h2>*/}
-            {/*      <p>Logout from your account</p>*/}
-            {/*    </a>*/}
-            {/*  )}*/}
-
-            {/*  {!session && (*/}
-            {/*    <a href="#" onClick={() => signIn("zitadel")}>*/}
-            {/*      <h2>Login &rarr;</h2>*/}
-            {/*      <p>Login with a ZITADEL account</p>*/}
-            {/*    </a>*/}
-            {/*  )}*/}
-            {/*</main>*/}
-        </>
-    );
+      {/*  {!session && (*/}
+      {/*    <a href="#" onClick={() => signIn("zitadel")}>*/}
+      {/*      <h2>Login &rarr;</h2>*/}
+      {/*      <p>Login with a ZITADEL account</p>*/}
+      {/*    </a>*/}
+      {/*  )}*/}
+      {/*</main>*/}
+    </>
+  );
 }
