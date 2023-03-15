a89f0326176cc539e806555736767ba10f9fc2a9
Milos Djuric <djuric@me.com>
2023-03-15 20:35:54 +0100

    feat(global): added smartive eslint-config

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,28 +1,32 @@
-import NextAuth from 'next-auth';
+import NextAuth from "next-auth";
 
 export default NextAuth({
   providers: [
     {
-      id: 'zitadel',
-      name: 'zitadel',
-      type: 'oauth',
-      version: '2',
-      wellKnown: 'https://cas-fee-advanced-ocvdad.zitadel.cloud/.well-known/openid-configuration',
-      clientId: '181236603920908545@cas_fee_adv_qwacker_prod',
+      id: "zitadel",
+      name: "zitadel",
+      type: "oauth",
+      version: "2",
+      wellKnown:
+        "https://cas-fee-advanced-ocvdad.zitadel.cloud/.well-known/openid-configuration",
+      clientId: "181236603920908545@cas_fee_adv_qwacker_prod",
       authorization: {
         params: {
-          scope: 'openid email profile',
+          scope: "openid email profile",
         },
       },
       idToken: true,
-      checks: ['pkce', 'state'],
+      checks: ["pkce", "state"],
       client: {
-        token_endpoint_auth_method: 'none',
+        token_endpoint_auth_method: "none",
       },
       async profile(profile) {
         return {
           id: profile.sub,
-          username: profile.preferred_username?.replace('@smartive.zitadel.cloud', ''),
+          username: profile.preferred_username?.replace(
+            "@smartive.zitadel.cloud",
+            ""
+          ),
         };
       },
     },
