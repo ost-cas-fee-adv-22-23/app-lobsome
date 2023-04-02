9792a044cc76864d24928c619542e1df5103c399
Alexander Widmer <alexander.widmer@me.com>
2023-04-02 17:02:35 +0200

    feat(services): added create-post function feat(services): changed fetch-post return interface to match the infiniteQueryInterface

--- services/fetch-posts.ts
+++ services/fetch-posts.ts
@@ -1,14 +1,34 @@
-import { PostInterface } from '../data/post.interface';
-import { mumbleApi } from './api-request';
+import { ApiPost, Post } from '../types/post';
+import { getRequest } from './request';
+import { ResponseInterface } from '../types/generic-response';
+import fetchUser from './fetch-user';
 
-export interface PostsResponse {
-  count: number;
-  data: PostInterface[];
+export interface PaginationParams {
+  offset: number;
+  limit: number;
 }
 
-export default async (): Promise<PostsResponse> => {
+export default async (token: string, { offset = 0, limit = 10 }: PaginationParams): Promise<ResponseInterface<Post>> => {
   try {
-    return mumbleApi.getWithoutAuth<PostsResponse>(`https://qwacker-api-http-prod-4cxdci3drq-oa.a.run.app/posts`);
+    const config: RequestInit = {
+      headers: {
+        Accept: 'application/json',
+        'content-type': 'application/json',
+      },
+    };
+
+    const postsResponse = await getRequest<ResponseInterface<ApiPost>>(
+      `https://qwacker-api-http-prod-4cxdci3drq-oa.a.run.app/posts?offset=${offset}&limit=${limit}`,
+      config
+    );
+    const posts: Post[] = await Promise.all(
+      postsResponse.data.map(async (item) => {
+        const user = await fetchUser(item.creator, token);
+        return { ...item, creator: user };
+      })
+    );
+
+    return { ...postsResponse, data: posts };
   } catch (e: any) {
     throw new Error(`Parsing posts error - ${e.message}`);
   }
--- types/generic-response.ts
+++ types/generic-response.ts
@@ -1,4 +1,6 @@
 export interface ResponseInterface<T> {
   data: T[];
   count: number;
+  next?: string;
+  previous?: string;
 }
