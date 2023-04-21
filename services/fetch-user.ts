1c158a75c86aaa4ff0a5e292a1e0b84d7372ef65
Alexander Widmer <alexander.widmer@me.com>
2023-04-21 23:26:16 +0200

    feat(my-profile): added cache for users

--- services/fetch-user.ts
+++ services/fetch-user.ts
@@ -1,8 +1,13 @@
 import { getRequest } from './request';
 import { User } from '../types/user';
+import { cachedUsers } from '../pages/_app';
 
 export default async (id: string, token: string | undefined): Promise<User> => {
   try {
+    if (cachedUsers[id]) {
+      return cachedUsers[id];
+    }
+
     const config: RequestInit = {
       headers: {
         Accept: 'application/json',
@@ -11,7 +16,9 @@ export default async (id: string, token: string | undefined): Promise<User> => {
       },
     };
 
-    return getRequest<User>(`https://qwacker-api-http-prod-4cxdci3drq-oa.a.run.app/users/${id}`, config);
+    const user = await getRequest<User>(`https://qwacker-api-http-prod-4cxdci3drq-oa.a.run.app/users/${id}`, config);
+    cachedUsers[id] = user;
+    return user;
   } catch (e: any) {
     throw new Error(`Parsing posts error - ${e.message}`);
   }
