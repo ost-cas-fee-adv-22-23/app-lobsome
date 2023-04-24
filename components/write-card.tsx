dd9630f77758b17dcb4d49e9be64a85f3de24cf1
Milos Djuric <djuric@me.com>
2023-04-24 23:57:08 +0200

    refactor(write-card): added file-name on upload

--- components/write-card.tsx
+++ components/write-card.tsx
@@ -9,6 +9,7 @@ import {
 import { FileUploadModal } from './modals/file-upload-modal';
 import useTranslation from 'next-translate/useTranslation';
 import { CreatePost } from '../types/post';
+import Image from 'next/image';
 
 type WriteCardProps = {
   onSend: (message: CreatePost) => void;
@@ -41,6 +42,9 @@ export const WriteCard = ({ onSend }: WriteCardProps) => {
       <div className="mt-8 mb-4">
         <Textarea placeholder={t('write-card.placeholder')} onChange={handleMessageChange} value={form.text} />
       </div>
+
+      <div className="font-bold my-5">{form.file?.name}</div>
+
       <div className="flex space-x-5">
         <Button onClick={() => setShowFileUploadModal(true)} color={ButtonColors.SLATE} fullWidth>
           {t('write-card.upload-image')} <SvgUpload />
